0dddc42fe20e7dec2ac07eb2ca2df3b115f48f29
Mario Hernández <mariojho@hotmail.com>
2021-07-08 07:21:26 +0200

    Adding files into the project

---
+++ BL.Capitales/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Capitales
+{
+    public class Class1
+    {
+    }
+}
